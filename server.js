e30cfd04fc9d525a50604391938b8fdf6912f479
Abhishek Shinde <ashinde04@entrata.com>
2024-10-05 02:24:30 +0530

    Refactor WebSocket message sending and handle client connection errors

--- server.js
+++ server.js
@@ -12,16 +12,16 @@ wss.on("listening", function listening() {
 
 // When any client connects to this server via WebSocket
 wss.on("connection", function connection(ws, req) {
-	ws.on("error", (err) => console.error(`${err.name}: ${err.message}`));
+	const channel = req.headers.channel || "general";
 
-	console.log("Client connected with channel ID:", req.headers.channel);
+	console.log("Client connected with channel:", channel);
 
 	// send new joining message to every other connected clients, excluding itself
 	wss.clients.forEach(function each(client) {
 		if (client !== ws && client.readyState === WebSocket.OPEN) {
 			client.send(
 				JSON.stringify({
-					channel: "general",
+					channel,
 					message: "A new user has joined the channel",
 				}),
 			);
@@ -30,7 +30,7 @@ wss.on("connection", function connection(ws, req) {
 
 	// when a message is received, broadcasting to every other connected WebSocket clients, excluding itself
 	ws.on("message", function message(data) {
-		console.log("broadcasting message:", JSON.parse(data.toString()));
+		console.log("Broadcasting:", JSON.parse(data.toString()));
 
 		wss.clients.forEach(function each(client) {
 			if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -38,4 +38,6 @@ wss.on("connection", function connection(ws, req) {
 			}
 		});
 	});
+
+	ws.on("error", (err) => console.error(`${err.name}: ${err.message}`));
 });
